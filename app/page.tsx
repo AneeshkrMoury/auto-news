e20bbbecac0915f97cd9707e30b2242e6a069485
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-09-06 18:46:45 +0530

    "added bew featc"

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
+import { fetchNews } from '../lib/fetchNews';
+
 export default function Home() {
+  fetchNews('sports').then(a => console.log(a.length, a[0]));
+  
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
